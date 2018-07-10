54a50e6d3e9bcba53c30eb600572dc3f1e0b0527
Thrawn997 <kyralak@gmail.com>
2018-07-10 16:25:33 -0700

    moved lua files

--- single-key-post.lua
+++
@@ -1,7 +0,0 @@
-
--- example HTTP POST script which demonstrates setting the
--- HTTP method, body, and adding a header
-
-wrk.method = "POST"
-wrk.body   = '{"plaintext" : "dGVldkV0R2ZodVV5Yk9zUA=="}'
-wrk.headers["Content-Type"] = "application/json"
